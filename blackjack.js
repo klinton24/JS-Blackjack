61dc046af0f3c045acd4d67d3066a1d9ff663cab
K Smith <klinton.jp@gmail.com>
2018-08-24 08:42:29 -0700

    created index and made cards into objects.

--- blackjack.js
+++ blackjack.js
@@ -1,12 +1,25 @@
-let deck = [];
 let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
 let cardValues = ['Ace', 'King', 'Queen', 'Jack', 'Ten', 'Nine', 'Eight', 'Seven', 'Six', 'Five', 'Four', 'Three', 'Two'];
 
-for (let suitIndex = 0; suitIndex < suits.length; suitIndex++) {
-    for (let cardValueIndex = 0; cardValueIndex < cardValues.length; cardValueIndex++) {
-        deck.push(cardValues[cardValueIndex] + " of " + suits[suitIndex]);
+function makeDeck() {
+    let deck = [];
+    for (let suitIndex = 0; suitIndex < suits.length; suitIndex++) {
+        for (let cardValueIndex = 0; cardValueIndex < cardValues.length; cardValueIndex++) {
+            //make each card an object
+            let card = {
+                suit: suits[suitIndex],
+                value: cardValues[cardValueIndex]
+            };
+            deck.push(card);
+        }
     }
 }
+
+function cardToString(card) {
+    return cardvalue + " of " + card.suit;
+}
+
+let deck = makeDeck();
 
 console.log(deck.length);
 console.log(deck);
